255549497c2ef9200ebe41c59f772041a8a1677e
Julian <Julian@JMONTESHPSPECTR>
2025-03-06 14:29:16 -0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,75 @@
+﻿using QuDevChallenge;
+
+//TestWords();
+TestMoreThan10();
+
+
+
+void TestWords()
+{
+    List<string> matrix = new List<string>();
+    List<string> words = new List<string>();
+
+    matrix.Add("abcdc");
+    matrix.Add("fgwio");
+    matrix.Add("chill");
+    matrix.Add("pqnsd");
+    matrix.Add("uvdxy");
+
+    words.Add("cold");
+    words.Add("wind");
+    words.Add("snow");
+    words.Add("chill");
+
+    WordFinder finder = new WordFinder(matrix);
+
+    IEnumerable<string> found = finder.Find(words);
+
+    foreach (string word in found)
+    {
+        Console.WriteLine(word);
+    }
+
+}
+
+void TestMoreThan10()
+{
+    List<string> matrix = new List<string>();
+    List<string> words = new List<string>();
+
+    matrix.Add("abcdcwindc");
+    matrix.Add("fgwioi234h");
+    matrix.Add("chilln234i");
+    matrix.Add("pqnsdd234l");
+    matrix.Add("uvdxy1234l");
+
+    matrix.Add("abcdchindc");
+    matrix.Add("fgwioa23wh");
+    matrix.Add("chilln23ii");
+    matrix.Add("Armsdd23nl");
+    matrix.Add("windy123dl");
+
+    words.Add("cold");
+    words.Add("wind");
+    words.Add("snow");
+    words.Add("chill");
+
+    words.Add("chair");
+    words.Add("arm");
+    words.Add("hand");
+    words.Add("chill");
+    words.Add("chill");
+    words.Add("chill");
+    words.Add("chill");
+    words.Add("chill");
+    words.Add("chill");
+
+    WordFinder finder = new WordFinder(matrix);
+
+    IEnumerable<string> found = finder.Find(words);
+
+    foreach (string word in found)
+    {
+        Console.WriteLine(word);
+    }
+}
---
+++ WordFinder.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuDevChallenge
+{
+    public class WordFinder
+    {
+        //  Each string represents a row in the matrix
+        private IEnumerable<string> _matrix;
+        // Represents the original matrix rotated as each row in this matrix is a column in the original matrix
+        private IEnumerable<string> _matrixRotated;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            _matrix = matrix;
+            List<string> rotated = new List<string>();
+
+            //Build Rotated Matrix
+            StringBuilder column;
+            for (int i = 0; i < _matrix.FirstOrDefault().Length; i++)
+            {
+                column = new StringBuilder();
+                foreach (string row in _matrix)
+                {
+                    column.Append(row[i]);
+                }
+
+                //If column is not empty, add it to the rotatedMatrix
+                if (!string.IsNullOrEmpty(column.ToString()))
+                    rotated.Add(column.ToString());
+            }
+            _matrixRotated = rotated;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            List<string> result = new List<string>();
+
+            List<WordsAndOcurrencies> found = FindAll(wordstream);
+
+            if (found.Count < 11)
+            {
+                foreach (var word in found)
+                {
+                    result.Add(word.Word);
+                }
+            }
+            else
+            {
+                found.Sort();
+                found.Reverse();
+
+                var toReturn = found.GetRange(0, 10);
+
+                foreach (var word in toReturn)
+                {
+                    result.Add(word.Word);
+                }
+            }
+
+            return result;
+        }
+
+        private List<WordsAndOcurrencies> FindAll(IEnumerable<string> wordstream)
+        {
+            List<WordsAndOcurrencies> found = new List<WordsAndOcurrencies>();
+            int ocurrencies = 0;
+            WordsAndOcurrencies wordAndOcurrencies;
+            foreach (string word in wordstream)
+            {
+                ocurrencies = 0;
+                ocurrencies = FindHorizontally(word) + FindVertically(word);
+
+                if (ocurrencies > 0)
+                {
+                    wordAndOcurrencies = new WordsAndOcurrencies(word, ocurrencies);
+
+                    found.Add(wordAndOcurrencies);
+                }
+            }
+
+            return found;
+        }
+
+        private int FindHorizontally(string word)
+        {
+            int ocurrencies = 0;
+
+            foreach (string row in _matrix)
+            {
+                if (row.ToLower().Contains(word.ToLower()))
+                    ocurrencies++;
+            }
+
+            return ocurrencies;
+        }
+
+        private int FindVertically(string word)
+        {
+            int ocurrencies = 0;
+
+            foreach (string row in _matrixRotated)
+            {
+                if (row.ToLower().Contains(word.ToLower()))
+                    ocurrencies++;
+            }
+
+            return ocurrencies;
+        }
+    }
+
+    class WordsAndOcurrencies : IComparable<WordsAndOcurrencies> 
+    {
+        public string Word { get; set; }
+        public int Ocurrencies { get; set; }
+
+        public WordsAndOcurrencies(string word, int ocurrencies)
+        {
+            Word = word; Ocurrencies = ocurrencies;
+        }
+
+        public int CompareTo(WordsAndOcurrencies? other)
+        {
+            if (Ocurrencies < other.Ocurrencies)
+                return -1;
+            if (Ocurrencies > other.Ocurrencies)
+                return 1;
+
+            return 0;
+
+        }
+    }
+}
