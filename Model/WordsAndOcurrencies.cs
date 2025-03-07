3634ae35d1b283315131fda015fc49b6a340b62b
Julian Montes <jmontes_ar@yahoo.com>
2025-03-07 20:02:18 -0300

    Removing Duplicated before return

---
+++ Model/WordsAndOcurrencies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuDevChallenge.Model
+{
+    public class WordsAndOcurrencies : IComparable<WordsAndOcurrencies>
+    {
+        public string Word { get; set; }
+        public int Occurrences { get; set; }
+
+        public WordsAndOcurrencies(string word, int ocurrencies)
+        {
+            Word = word; Occurrences = ocurrencies;
+        }
+
+        public int CompareTo(WordsAndOcurrencies? other)
+        {
+            if (Occurrences < other.Occurrences)
+                return -1;
+            if (Occurrences > other.Occurrences)
+                return 1;
+
+            return 0;
+        }
+    }
+}
